d6c251093307a7b8fc47ae7ab70ac3f50cf97f4f
Tengill91 <robban_t@hotmail.com>
2021-09-17 14:10:31 +0200

    första committen/Robban

---
+++ src/main/java/com/grupp6/grupp6kontosystem/Grupp6kontosystemApplication.java
@@ -0,0 +1,13 @@
+package com.grupp6.grupp6kontosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp6kontosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupp6kontosystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupp6/grupp6kontosystem/Grupp6kontosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp6.grupp6kontosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp6kontosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
